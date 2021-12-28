08ae5027dd22e3f98c65d49b08f5c64b825e4aa3
JBeni <JBeni@users.noreply.github.com>
2021-12-28 12:12:53 +0200

    creating the application DB context

---
+++ Blazor.Data/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Blazor.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blazor.Data.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Product> Products { get; set; }
+        DbSet<City> Cities { get; set; }
+        DbSet<Employee> Employees { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
