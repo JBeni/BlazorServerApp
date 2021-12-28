fb9ef4779ea972496bc348a8236eb28852feac7f
JBeni <JBeni@users.noreply.github.com>
2021-12-29 00:18:44 +0200

    create Refresh request model

---
+++ Blazor.Data/Models/RefreshRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Blazor.Data.Models
+{
+    public class RefreshRequest
+    {
+        public string? AccessToken { get; set; }
+        public string? RefreshToken { get; set; }
+    }
+}
