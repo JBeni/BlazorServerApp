762d14adff3799ae9bddf06135248d607b08cad9
JBeni <JBeni@users.noreply.github.com>
2021-12-29 00:18:18 +0200

    create Jwt Service

---
+++ Blazor.Data/Interfaces/IJwtService.cs
@@ -0,0 +1,9 @@
+﻿namespace Blazor.Data.Interfaces
+{
+    public interface IJwtService
+    {
+        Task<string> GetTokenAsync();
+        Task<bool> SaveTokenAsync(string Token);
+        Task<bool> DestroyTokenAsync();
+    }
+}
