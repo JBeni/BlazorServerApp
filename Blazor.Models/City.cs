75f377f9d773673b5813ae2c403ff182e3fb9236
JBeni <JBeni@users.noreply.github.com>
2021-12-28 12:05:58 +0200

    create entities for Database

---
+++ Blazor.Models/City.cs
@@ -0,0 +1,8 @@
+﻿namespace Blazor.Models
+{
+    public partial class City
+    {
+        public int CityId { get; set; }
+        public string? CityName { get; set; }
+    }
+}
---
+++ Blazor.Models/Product.cs
@@ -0,0 +1,9 @@
+﻿using Blazor.Models.Common;
+
+namespace Blazor.Models
+{
+    public class Product : EntityBase
+    {
+        public string? Name { get; set; }
+    }
+}
