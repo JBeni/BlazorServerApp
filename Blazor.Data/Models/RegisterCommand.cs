d95bb608f5443fe963c8f6fa0f6d1305745348c8
JBeni <JBeni@users.noreply.github.com>
2021-12-28 12:16:25 +0200

    create register command model

---
+++ Blazor.Data/Models/RegisterCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace Blazor.Data.Models
+{
+    public class RegisterCommand
+    {
+        public string? Email { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Password { get; set; }
+        public string? ConfirmPassword { get; set; }
+    }
+}
