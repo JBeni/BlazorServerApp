4918e1b0c3b76a03f11141397b6e1d55f6bfe22f
JBeni <JBeni@users.noreply.github.com>
2021-12-28 12:16:44 +0200

    create role response model

---
+++ Blazor.Data/Responses/AppRoleResponse.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Blazor.Data.Mappings;
+using Blazor.Models.Identity;
+
+namespace Blazor.Data.Responses
+{
+    public class AppRoleResponse : IMapFrom<AppRole>
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? NormalizedName { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<AppRole, AppRoleResponse>()
+                .ForMember(d => d.Id, opt => opt.MapFrom(s => s.Id))
+                .ForMember(d => d.Name, opt => opt.MapFrom(s => s.Name))
+                .ForMember(d => d.NormalizedName, opt => opt.MapFrom(s => s.NormalizedName));
+        }
+
+        public string? Error { get; set; }
+    }
+}
