7f4da40a9ecc1cc084f1fe6b387fdc16ede1e59f
JBeni <JBeni@users.noreply.github.com>
2021-12-28 12:15:00 +0200

    creating the Employee Services

---
+++ Blazor.Data/Services/EmployeeDataAccessLayer.cs
@@ -0,0 +1,107 @@
+﻿using Blazor.Data.Interfaces;
+using Blazor.Data.Persistence;
+using Blazor.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blazor.Data.Services
+{
+    public class EmployeeDataAccessLayer : IEmployeeService
+    {
+        private ApplicationDbContext db;
+
+        public EmployeeDataAccessLayer(ApplicationDbContext _db)
+        {
+            db = _db;
+        }
+
+        public List<Employee> GetAllEmployees()
+        {
+            try
+            {
+                return db.Employees.AsNoTracking().ToList();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public void AddEmployee(Employee employee)
+        {
+            try
+            {
+                db.Employees.Add(employee);
+                db.SaveChanges();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public void UpdateEmployee(Employee employee)
+        {
+            try
+            {
+                db.Entry(employee).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public Employee GetEmployeeData(int id)
+        {
+            try
+            {
+                Employee? employee = db.Employees.Find(id);
+
+                if (employee != null)
+                {
+                    db.Entry(employee).State = EntityState.Detached;
+                    return employee;
+                }
+                else
+                {
+                    throw new ArgumentNullException();
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public void DeleteEmployee(int id)
+        {
+            try
+            {
+                Employee? employee = db.Employees.Find(id);
+
+                if (employee != null)
+                {
+                    db.Employees.Remove(employee);
+                }
+                db.SaveChanges();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public List<City> GetCityData()
+        {
+            try
+            {
+                return db.Cities.ToList();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ Blazor.Data/Services/EmployeeService.cs
@@ -0,0 +1,44 @@
+﻿using Blazor.Data.Interfaces;
+using Blazor.Models;
+
+namespace Blazor.Data.Services
+{
+    public class EmployeeService
+    {
+        private readonly IEmployeeService objemployee;
+
+        public EmployeeService(IEmployeeService _objemployee)
+        {
+            objemployee = _objemployee;
+        }
+
+        public Task<List<Employee>> GetEmployeeList()
+        {
+            return Task.FromResult(objemployee.GetAllEmployees());
+        }
+
+        public void Create(Employee employee)
+        {
+            objemployee.AddEmployee(employee);
+        }
+        public Task<Employee> Details(int id)
+        {
+            return Task.FromResult(objemployee.GetEmployeeData(id));
+        }
+
+        public void Edit(Employee employee)
+        {
+            objemployee.UpdateEmployee(employee);
+        }
+
+        public void Delete(int id)
+        {
+            objemployee.DeleteEmployee(id);
+        }
+        public Task<List<City>> GetCities()
+        {
+            return Task.FromResult(objemployee.GetCityData());
+        }
+    }
+
+}
