8d85efb28a0347d9b6347a541ecb8cfb65779669
JBeni <JBeni@users.noreply.github.com>
2021-12-28 12:15:35 +0200

    create custom auth provider

---
+++ Blazor.Server/Filters/CustomAuthStateProvider.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Blazor.Server.Filters
+{
+    public class CustomAuthStateProvider : AuthenticationStateProvider
+    {
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var identity = new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.Name, "mrfibuli"),
+            }, "Fake authentication type");
+
+            var user = new ClaimsPrincipal(identity);
+
+            return Task.FromResult(new AuthenticationState(user));
+        }
+
+        Claim _claim;
+
+        public AuthorizationUserFilter() { }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var hasClaim = context.HttpContext.User.Claims.Any(c => c.Type == _claim.Type && c.Value == _claim.Value);
+            if (!hasClaim)
+            {
+                context.Result = new ForbidResult();
+            }
+        }
+    }
+}
