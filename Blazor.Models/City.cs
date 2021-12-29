513cef95e43562b87cfe0b04dd8e7590edbacdd1
JBeni <JBeni@users.noreply.github.com>
2021-12-29 20:00:23 +0200

    update City entity model

--- Blazor.Models/City.cs
+++ Blazor.Models/City.cs
@@ -1,7 +1,12 @@
-﻿namespace Blazor.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Blazor.Models
 {
     public class City
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int CityId { get; set; }
         public string? CityName { get; set; }
     }
