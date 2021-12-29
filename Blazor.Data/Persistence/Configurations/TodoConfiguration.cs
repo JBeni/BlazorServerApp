a1c059ffe5c14d4acec5e85ec3078e87e28d2f9e
JBeni <JBeni@users.noreply.github.com>
2021-12-29 20:05:42 +0200

    create Todo configurations for entity DB

---
+++ Blazor.Data/Persistence/Configurations/TodoConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Blazor.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Blazor.Data.Persistence.Configurations
+{
+    public class TodoConfiguration : IEntityTypeConfiguration<Todo>
+    {
+        public void Configure(EntityTypeBuilder<Todo> builder)
+        {
+            builder.ToTable("Todos");
+            
+            builder.Property(t => t.Title)
+                .HasMaxLength(200)
+                .IsRequired();
+        }
+    }
+}
