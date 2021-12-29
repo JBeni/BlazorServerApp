7a16d9203009620a54b5de83a233ea261f96f938
JBeni <JBeni@users.noreply.github.com>
2021-12-29 20:02:00 +0200

    update todo service interface

---
+++ Blazor.Data/Interfaces/ITodoService.cs
@@ -0,0 +1,13 @@
+﻿using Blazor.Data.Responses;
+
+namespace Blazor.Data.Interfaces
+{
+    public interface ITodoService
+    {
+        Task CreateTodo(TodoResponse todo);
+        Task CompleteTodo(int todoId);
+
+        public List<TodoResponse> GetAllTodos();
+        public TodoResponse GetTodoData(int id);
+    }
+}
