4ee1549c4c1381e0c3dfb29d38cf2cd458d67280
JBeni <JBeni@users.noreply.github.com>
2021-12-29 20:01:52 +0200

    update employee service interface

--- Blazor.Data/Interfaces/IEmployeeService.cs
+++ Blazor.Data/Interfaces/IEmployeeService.cs
@@ -1,15 +1,14 @@
 ﻿using Blazor.Data.Responses;
-using Blazor.Models;
 
 namespace Blazor.Data.Interfaces
 {
     public interface IEmployeeService
     {
         public List<EmployeeResponse> GetAllEmployees();
-        public void AddEmployee(EmployeeResponse employee);
-        public void UpdateEmployee(EmployeeResponse employee);
+        public Task AddEmployee(EmployeeResponse employee);
+        public Task UpdateEmployee(EmployeeResponse employee);
         public EmployeeResponse GetEmployeeData(int id);
-        public void DeleteEmployee(int id);
+        public Task DeleteEmployee(int id);
         public List<CityResponse> GetCitiesData();
     }
 }
