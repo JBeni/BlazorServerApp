3f169903d299e2738d384bd44fc4b43571e73308
JBeni <JBeni@users.noreply.github.com>
2021-12-30 23:36:20 +0200

    product response from decimal to int

--- Blazor.Data/Responses/ProductResponse.cs
+++ Blazor.Data/Responses/ProductResponse.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string? Name { get; set; } = null!;
         public string? Description { get; set; } = null!;
-        public decimal? Price { get; set; }
+        public int Price { get; set; }
         public int Amount { get; set; }
         public string? Brand { get; set; } = null!;
         public int SizeId { get; set; }
