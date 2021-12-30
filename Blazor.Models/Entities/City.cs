c8a3a3dbca73597899d84b251463a4fa0511e307
JBeni <JBeni@users.noreply.github.com>
2021-12-30 20:12:32 +0200

    create city entity DB

---
+++ Blazor.Models/Entities/City.cs
@@ -0,0 +1,10 @@
+﻿namespace Blazor.Models.Entities
+{
+    public class City
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CityId { get; set; }
+        public string? CityName { get; set; }
+    }
+}
