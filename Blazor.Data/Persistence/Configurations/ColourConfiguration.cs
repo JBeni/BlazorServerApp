dad1a7c7d7d6195c04089af9e7bef3289e2350de
JBeni <JBeni@users.noreply.github.com>
2021-12-30 14:24:56 +0200

    update Colour Entity configuration

---
+++ Blazor.Data/Persistence/Configurations/ColourConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Blazor.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Blazor.Data.Persistence.Configurations
+{
+    public class ColourConfiguration : IEntityTypeConfiguration<Colour>
+    {
+        public void Configure(EntityTypeBuilder<Colour> builder)
+        {
+            builder.ToTable("Colours");
+
+            builder.HasKey(x => x.Id);
+            builder.Property(p => p.Id).ValueGeneratedNever();
+            builder.Property(e => e.Id).HasColumnName("Id");
+            builder.Property(e => e.Name)
+                .HasMaxLength(150)
+                .IsUnicode(false);
+        }
+    }
+}
