abc97dd457b1af47e8a0d44305bd520de6968b72
JBeni <JBeni@users.noreply.github.com>
2021-12-30 14:28:10 +0200

    create AuthenticationUserModel

---
+++ Blazor.Server/Others/AuthenticationUserModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blazor.Server.Others
+{
+    public class AuthenticationUserModel
+    {
+        [Required(ErrorMessage = "Email Address is required.")]
+        public string? Email { get; set;}
+        [Required(ErrorMessage = "Password is required.")]
+        public string? Password { get; set;}
+    }
+}
