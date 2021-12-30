28a60240afe7180d0c8694d662448d5cd7d34630
JBeni <JBeni@users.noreply.github.com>
2021-12-30 20:13:23 +0200

    create todo entity DB

---
+++ Blazor.Models/Entities/Todo.cs
@@ -0,0 +1,8 @@
+﻿namespace Blazor.Models.Entities
+{
+    public class Todo : EntityBase
+    {
+        public string Title { get; set; } = null!;
+        public bool IsCompleted { get; set; } = false;
+    }
+}
