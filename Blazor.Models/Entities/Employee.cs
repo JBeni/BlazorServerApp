d0e69130a9f1316bc87cb0e5e6412523da565d2f
JBeni <JBeni@users.noreply.github.com>
2021-12-30 20:12:54 +0200

    create employee entity DB

---
+++ Blazor.Models/Entities/Employee.cs
@@ -0,0 +1,20 @@
+﻿namespace Blazor.Models.Entities
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+
+        [Required]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public string City { get; set; } = null!;
+
+        [Required]
+        public string Department { get; set; } = null!;
+
+        [Required]
+        public string Gender { get; set; } = null!;
+    }
+}
