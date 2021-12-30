316f03931edcde4588e5b325f5b8a6953041568d
JBeni <JBeni@users.noreply.github.com>
2021-12-30 23:35:47 +0200

    add annotation to Login command

--- Blazor.Data/Models/LoginCommand.cs
+++ Blazor.Data/Models/LoginCommand.cs
@@ -2,7 +2,9 @@
 {
     public class LoginCommand
     {
+        [Required(ErrorMessage = "Email Address is required.")]
         public string? Email { get; set; }
+        [Required(ErrorMessage = "Password is required.")]
         public string? Password { get; set; }
     }
 }
