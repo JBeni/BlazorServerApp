365eacb459b0c6e38164c1318f90db3c8e96279e
JBeni <JBeni@users.noreply.github.com>
2021-12-30 20:09:56 +0200

    create global usings - blazor models project

---
+++ Blazor.Models/GlobalUsings.cs
@@ -0,0 +1,4 @@
+﻿global using Microsoft.AspNetCore.Identity;
+global using Blazor.Models.Common;
+global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;
