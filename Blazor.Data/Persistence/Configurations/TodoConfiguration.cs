70f500cf8e6ecaf5d436e8bf49bc259c30a692ad
JBeni <JBeni@users.noreply.github.com>
2021-12-30 14:25:28 +0200

    update Todo Entity configuration

--- Blazor.Data/Persistence/Configurations/TodoConfiguration.cs
+++ Blazor.Data/Persistence/Configurations/TodoConfiguration.cs
@@ -9,7 +9,8 @@ namespace Blazor.Data.Persistence.Configurations
         public void Configure(EntityTypeBuilder<Todo> builder)
         {
             builder.ToTable("Todos");
-            
+
+            builder.HasKey(x => x.Id);
             builder.Property(t => t.Title)
                 .HasMaxLength(200)
                 .IsRequired();
