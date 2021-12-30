3e0febc27e175d17e91282f8a001163f93293c7c
JBeni <JBeni@users.noreply.github.com>
2021-12-30 14:25:20 +0200

    update Size Entity configuration

---
+++ Blazor.Data/Persistence/Configurations/SizeConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Blazor.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Blazor.Data.Persistence.Configurations
+{
+    public class SizeConfiguration : IEntityTypeConfiguration<Size>
+    {
+        public void Configure(EntityTypeBuilder<Size> builder)
+        {
+            builder.ToTable("Sizes");
+
+            builder.HasKey(x => x.Id);
+            builder.Property(p => p.Id).ValueGeneratedNever();
+            builder.Property(e => e.Id).HasColumnName("Id");
+            builder.Property(e => e.Name)
+                .HasMaxLength(150)
+                .IsUnicode(false);
+        }
+    }
+}
