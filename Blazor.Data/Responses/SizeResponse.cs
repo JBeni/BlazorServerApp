689e517e5d5121d286a6e17308925d859b075278
JBeni <JBeni@users.noreply.github.com>
2021-12-30 20:10:51 +0200

    create size entity DB

---
+++ Blazor.Data/Responses/SizeResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace Blazor.Data.Responses
+{
+    public class SizeResponse : IMapFrom<Size>
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Size, SizeResponse>()
+                .ForMember(d => d.Id, opt => opt.MapFrom(s => s.Id))
+                .ForMember(d => d.Name, opt => opt.MapFrom(s => s.Name));
+        }
+
+        public string? Error { get; set; }
+    }
+}
