58382abec18b06814d8de455b686552113a2e67b
JBeni <JBeni@users.noreply.github.com>
2021-12-30 20:13:04 +0200

    create product entity DB

---
+++ Blazor.Models/Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace Blazor.Models.Entities
+{
+    public class Product : EntityBase
+    {
+        public string? Name { get; set; } = null!;
+        public string? Description { get; set; } = null!;
+        public decimal? Price { get; set; } = null!;
+        public int Amount { get; set;}
+        public string? Brand { get; set; } = null!;
+
+        public Size? Size { get; set; }
+        public Colour? Colour { get; set; }
+    }
+}
