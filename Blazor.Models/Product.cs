d3ff5904163edf05df8b84862b73e106a8a21201
JBeni <JBeni@users.noreply.github.com>
2021-12-30 14:25:52 +0200

    update Product Entity

--- Blazor.Models/Product.cs
+++ Blazor.Models/Product.cs
@@ -4,6 +4,13 @@ namespace Blazor.Models
 {
     public class Product : EntityBase
     {
-        public string? Name { get; set; }
+        public string? Name { get; set; } = null!;
+        public string? Description { get; set; } = null!;
+        public decimal? Price { get; set; } = null!;
+        public decimal? Amount { get; set;} = null!;
+        public string? Brand { get; set; } = null!;
+
+        public Size? Size { get; set; }
+        public Colour? Colour { get; set; }
     }
 }
