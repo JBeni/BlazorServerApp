a726abb54e1ae36a47f3b38bb2a7e0cb553ef9c3
JBeni <JBeni@users.noreply.github.com>
2021-12-30 23:37:06 +0200

    add annotations to global usings

--- Blazor.Data/GlobalUsings.cs
+++ Blazor.Data/GlobalUsings.cs
@@ -24,3 +24,4 @@ global using System.IdentityModel.Tokens.Jwt;
 global using System.Security.Claims;
 global using System.Text;
 global using System.Net.Http.Headers;
+global using System.ComponentModel.DataAnnotations;
