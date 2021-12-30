cec10e027f056fdcd54d8ef6dc4e73ee4330c9a4
JBeni <JBeni@users.noreply.github.com>
2021-12-30 20:17:02 +0200

    update authentication service

--- Blazor.Server/Others/AuthenticationService.cs
+++ Blazor.Server/Others/AuthenticationService.cs
@@ -1,56 +1,50 @@
-﻿using Blazored.LocalStorage;
-using Microsoft.AspNetCore.Components.Authorization;
-using System.Net.Http.Headers;
-using System.Text.Json;
+﻿namespace Blazor.Server.Others;
 
-namespace Blazor.Server.Others
+public class AuthenticationService : IAuthenticationService
 {
-    public class AuthenticationService : IAuthenticationService
+    private readonly HttpClient _httpClient;
+    private readonly AuthenticationStateProvider _authStateProvider;
+    private readonly ILocalStorageService _localStorage;
+
+    public AuthenticationService(HttpClient httpClient, AuthenticationStateProvider authStateProvider, ILocalStorageService localStorage)
     {
-        private readonly HttpClient _httpClient;
-        private readonly AuthenticationStateProvider _authStateProvider;
-        private readonly ILocalStorageService _localStorage;
+        _httpClient = httpClient;
+        _authStateProvider = authStateProvider;
+        _localStorage = localStorage;
+    }
 
-        public AuthenticationService(HttpClient httpClient, AuthenticationStateProvider authStateProvider, ILocalStorageService localStorage)
+    public async Task<AuthenticatedUserModel> Login(AuthenticationUserModel userForAuthenticatrion)
+    {
+        var data = new FormUrlEncodedContent(new[]
         {
-            _httpClient = httpClient;
-            _authStateProvider = authStateProvider;
-            _localStorage = localStorage;
+            new KeyValuePair<string, string>("grant_type", "password"),
+            new KeyValuePair<string, string>("username", userForAuthenticatrion.Email),
+            new KeyValuePair<string, string>("password", userForAuthenticatrion.Password)
+        });
+        var authResult = await _httpClient.PostAsync("https://localhost:5001/token", data);
+        var authContent = await authResult.Content.ReadAsStringAsync();
+
+        if (authResult.IsSuccessStatusCode == false)
+        {
+            return null;
         }
 
-        public async Task<AuthenticatedUserModel> Login(AuthenticationUserModel userForAuthenticatrion)
-        {
-            var data = new FormUrlEncodedContent(new[]
-            {
-                new KeyValuePair<string, string>("grant_type", "password"),
-                new KeyValuePair<string, string>("username", userForAuthenticatrion.Email),
-                new KeyValuePair<string, string>("password", userForAuthenticatrion.Password)
-            });
-            var authResult = await _httpClient.PostAsync("https://localhost:5001/token", data);
-            var authContent = await authResult.Content.ReadAsStringAsync();
+        var result = JsonSerializer.Deserialize<AuthenticatedUserModel>(
+            authContent,
+            new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
 
-            if (authResult.IsSuccessStatusCode == false)
-            {
-                return null;
-            }
+        await _localStorage.SetItemAsync("authToken", result.Access_Token);
+        ((AuthStateProvider)_authStateProvider).NotifyUserAuthentication(result.Access_Token);
 
-            var result = JsonSerializer.Deserialize<AuthenticatedUserModel>(
-                authContent,
-                new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", result.Access_Token);
 
-            await _localStorage.SetItemAsync("authToken", result.Access_Token);
-            ((AuthStateProvider)_authStateProvider).NotifyUserAuthentication(result.Access_Token);
+        return result;
+    }
 
-            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", result.Access_Token);
-
-            return result;
-        }
-
-        public async Task Logout()
-        {
-            await _localStorage.RemoveItemAsync("authToken");
-            ((AuthStateProvider)_authStateProvider).NotifyUserLogout();
-            _httpClient.DefaultRequestHeaders.Authorization = null;
-        }
+    public async Task Logout()
+    {
+        await _localStorage.RemoveItemAsync("authToken");
+        ((AuthStateProvider)_authStateProvider).NotifyUserLogout();
+        _httpClient.DefaultRequestHeaders.Authorization = null;
     }
 }
