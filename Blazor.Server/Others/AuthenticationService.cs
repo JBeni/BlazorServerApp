c3ba9bf10c67006b8a9fae1625e90f3ccbb2c96c
JBeni <JBeni@users.noreply.github.com>
2021-12-30 14:27:55 +0200

    create AuthenticationService

---
+++ Blazor.Server/Others/AuthenticationService.cs
@@ -0,0 +1,56 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace Blazor.Server.Others
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly AuthenticationStateProvider _authStateProvider;
+        private readonly ILocalStorageService _localStorage;
+
+        public AuthenticationService(HttpClient httpClient, AuthenticationStateProvider authStateProvider, ILocalStorageService localStorage)
+        {
+            _httpClient = httpClient;
+            _authStateProvider = authStateProvider;
+            _localStorage = localStorage;
+        }
+
+        public async Task<AuthenticatedUserModel> Login(AuthenticationUserModel userForAuthenticatrion)
+        {
+            var data = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("grant_type", "password"),
+                new KeyValuePair<string, string>("username", userForAuthenticatrion.Email),
+                new KeyValuePair<string, string>("password", userForAuthenticatrion.Password)
+            });
+            var authResult = await _httpClient.PostAsync("https://localhost:5001/token", data);
+            var authContent = await authResult.Content.ReadAsStringAsync();
+
+            if (authResult.IsSuccessStatusCode == false)
+            {
+                return null;
+            }
+
+            var result = JsonSerializer.Deserialize<AuthenticatedUserModel>(
+                authContent,
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            await _localStorage.SetItemAsync("authToken", result.Access_Token);
+            ((AuthStateProvider)_authStateProvider).NotifyUserAuthentication(result.Access_Token);
+
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", result.Access_Token);
+
+            return result;
+        }
+
+        public async Task Logout()
+        {
+            await _localStorage.RemoveItemAsync("authToken");
+            ((AuthStateProvider)_authStateProvider).NotifyUserLogout();
+            _httpClient.DefaultRequestHeaders.Authorization = null;
+        }
+    }
+}
---
+++ Blazor.Server/Others/IAuthenticationService.cs
@@ -0,0 +1,8 @@
+﻿namespace Blazor.Server.Others
+{
+    public interface IAuthenticationService
+    {
+        Task<AuthenticatedUserModel> Login(AuthenticationUserModel userForAuthenticatrion);
+        Task Logout();
+    }
+}
