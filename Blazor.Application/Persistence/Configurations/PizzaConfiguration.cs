4a320cb1c90e9e436aa61e25e476c15c6668fa91
JBeni <JBeni@users.noreply.github.com>
2021-12-31 14:34:48 +0200

    create pizza entity configuration

---
+++ Blazor.Application/Persistence/Configurations/PizzaConfiguration.cs
@@ -0,0 +1,16 @@
+﻿namespace Blazor.Application.Persistence.Configurations
+{
+    public class PizzaConfiguration : IEntityTypeConfiguration<Pizza>
+    {
+        public void Configure(EntityTypeBuilder<Pizza> builder)
+        {
+            builder.ToTable("Pizzas");
+
+            builder.HasKey(x => x.Id);
+            //builder.Property(p => p.Id).ValueGeneratedNever();
+            builder.Property(t => t.Name)
+                .HasMaxLength(200)
+                .IsRequired();
+        }
+    }
+}
