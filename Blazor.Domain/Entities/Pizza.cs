8b5d785907f05f43d6904cd6ffbf66812bdb9018
JBeni <JBeni@users.noreply.github.com>
2021-12-31 14:34:39 +0200

    create pizza entity

---
+++ Blazor.Domain/Entities/Pizza.cs
@@ -0,0 +1,12 @@
+﻿namespace Blazor.Domain.Entities
+{
+    public class Pizza : EntityBase
+    {
+        public string? Name { get; set; } = null!;
+        public string? Description { get; set; } = null!;
+        public string? Size { get; set; } = null!;
+        public int Price { get; set; }
+        public string? ImagePath { get; set; } = null!;
+        public string? ImageName { get; set; } = null!;
+    }
+}
