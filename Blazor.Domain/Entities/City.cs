894382fe3774f39dd151e3c2a785757402649efd
JBeni <JBeni@users.noreply.github.com>
2021-12-31 14:34:20 +0200

    remove value generated never from city entity

--- Blazor.Domain/Entities/City.cs
+++ Blazor.Domain/Entities/City.cs
@@ -3,7 +3,6 @@
     public class City
     {
         [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int CityId { get; set; }
         public string? CityName { get; set; }
     }
