93e1a469db66aabdc05eda3036a07089678358ce
JBeni <JBeni@users.noreply.github.com>
2022-01-01 22:21:41 +0200

    remove files

--- Blazor.Application/Mappings/IMapFrom.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Blazor.Application.Mappings
-{
-    public interface IMapFrom<T>
-    {
-        void Mapping(Profile profile) => profile.CreateMap(typeof(T), GetType());
-    }
-}
--- Blazor.Application/Mappings/MappingExtensions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Blazor.Application.Mappings
-{
-    public static class MappingExtensions
-    {
-        public static Task<List<TDestination>> ProjectToListAsync<TDestination>(this IQueryable queryable, AutoMapper.IConfigurationProvider configuration)
-            => queryable.ProjectTo<TDestination>(configuration).ToListAsync();
-    }
-}
